06bcc0f114c9244e9c602a231311ee331c9b32a9
ziyahingora <93203286+ziyahingora@users.noreply.github.com>
2022-09-13 16:55:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+leftWrist = "";
+rightWrist = "";
+leftWristX = "";
+rightWristX = "";
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 550);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    background('#969A97');
+    textSize(difference);
+    fill('#FFE787');
+    text('ziya', 50, 400);
+}
+
+function modelLoaded() 
+{
+    console.log('PoseNet Is Initialized!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX = rightWristX);
+    }
+}
